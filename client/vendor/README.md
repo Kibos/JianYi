6041ab2b55c2343d2f4e0772fe9672cb6bba49d1
ybyGold <415098820@qq.com>
2016-08-18 23:36:46 +0800

    init

---
+++ client/vendor/README.md
@@ -0,0 +1,13 @@
+## Client
+
+This is the place for your application front-end files.
+
+外部引入
+
+==============
+angular-min.js
+
+==============
+
+http://www.tuicool.com/articles/zeiy6ff
+https://angular-ui.github.io/ui-router/site/#/api/ui.router
--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,6 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+  // router.get('/', server.loopback.status());  //  This code says that for any GET request to the root URI ("/"), the application will return the results of loopback.status().
+  // server.use(router);
 };
