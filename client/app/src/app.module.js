b6b6eef649594ef0dd3c02ba03a12d9fffe3f00b
Kibos <759508922@qq.com>
2016-08-19 17:52:29 +0800

    aaaaaaaaa

---
+++ client/app/src/app.module.js
@@ -0,0 +1,14 @@
+// Copyright IBM Corp. 2015. All Rights Reserved.
+// Node module: loopback-getting-started-intermediate
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+(function() {
+    'use strict';
+angular
+  .module('app', [
+    'ui.router',
+    'lbServices',
+    'app.account'
+  ])
+
+})();
